41372adcbc990d935f49c8d03570ecf5f582a6b7
kkhalil <k.khalil@satzmedia.de>
2020-03-04 11:45:34 +0100

    updated script to set {"stringMap": null} instead null in Description key.

--- xmlToJson.py
+++ xmlToJson.py
@@ -17,12 +17,14 @@ def setValueInKeyIfhasSameName(dictData) -> dict:
                     dictData[key] = value
                 if len(value.keys()) == 1 and "string" in value.keys():
                     dictData[key] = value["string"]
-                    if key == "Description" and isinstance(dictData[key], dict)  and"value" in dictData[key].keys():
+                    if key == "Description" and isinstance(dictData[key], dict)  and "value" in dictData[key].keys():
                         stringMap = {"stringMap" : {}}
                         languageValues = dictData[key]["value"]
                         for languageValue in languageValues:
                             if "language" in languageValue and "text" in languageValue:
                                 stringMap["stringMap"][languageValue["language"]] = languageValue["text"]
+                        if len(stringMap["stringMap"].keys()) < 1:
+                            stringMap["stringMap"] = None
                         dictData[key] = stringMap
                 if key == "Name" and "string" in value.keys() and isinstance(value["string"], dict) and "value" in value["string"].keys():
                     stringMap = {"stringMap" : {}}
@@ -41,6 +43,12 @@ def setValueInKeyIfhasSameName(dictData) -> dict:
                             setValueInKeyIfhasSameName(element)
                     if key == str(valueKey) + "s" or (key == "Categories" and valueKey == "Category"):
                         dictData[key] =  dictData[key][valueKey]
+                if key == "FieldTypes":
+                    for fieldType in dictData[key]:
+                        print(fieldType)
+                        if isinstance(fieldType, dict) and "Description" in fieldType.keys() and fieldType["Description"] is None:
+                            fieldType["Description"] = {"stringMap" : None}
+
             if isinstance(value, dict) and len(value.values()) ==  1 and "text" in value.keys():
                 dictData[key] = dictData[key]["text"]
 
@@ -52,23 +60,20 @@ def setValueInKeyIfhasSameName(dictData) -> dict:
 xmlFilePath = str(input(".XML file path (e.g.: path/to/your/file.xml): "))
 pathToSaveJsonFile = str(input(".JSON file path (e.g.: path/to/your/file.json): "))
 if xmlFilePath != "" and pathToSaveJsonFile != "":
-    try:
-        with open(xmlFilePath, 'r') as fd:
-            xmlData = fd.read()
-            xmlToJson = json.loads(json.dumps(xmltodict.parse(
-                xmlData, 
-                force_cdata=True, 
-                attr_prefix='', 
-                cdata_key='text', 
-                #force_list={'Key': 'text', 'Value': 'text'}
-            )
-            ))
-            data = setValueInKeyIfhasSameName(xmlToJson.get("Model"))
-            model_data = {}
-            with open(pathToSaveJsonFile, 'w') as outfile:
-                model_data["Model"] = data
-                json.dump(model_data, outfile)
-    except:
-        print("Cannot open xml file")
+    with open(xmlFilePath, 'r') as fd:
+        xmlData = fd.read()
+        xmlToJson = json.loads(json.dumps(xmltodict.parse(
+            xmlData, 
+            force_cdata=True, 
+            attr_prefix='', 
+            cdata_key='text', 
+        )
+        ))
+        data = setValueInKeyIfhasSameName(xmlToJson.get("Model"))
+        model_data = {}
+        with open(pathToSaveJsonFile, 'w') as outfile:
+            model_data = data
+            json.dump(model_data, outfile)
+
 else:
     print("Please set files paths")
